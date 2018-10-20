6b1a38325f320d6b819d1c6564d11161bd0e8674
Joey Steele <joeysteele2001@gmail.com>
2018-10-20 12:59:12 -0400

    Move VuforiaBuilder to its own class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/vuforia/VuforiaBuilder.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.competition.vuforia;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class VuforiaBuilder {
+
+    private VuforiaLocalizer.CameraDirection cameraDirection;
+    private boolean enableCameraMonitoring = false;
+    private OpenGLMatrix cameraLocation;
+    private String vuforiaKey;
+    private String vuforiaAssetName;
+    private List<String> vuforiaTrackableNames;
+    private HardwareMap hardwareMap;
+
+    public VuforiaBuilder(String vuforiaKey) {
+        this.vuforiaKey = vuforiaKey;
+    }
+
+    public VuforiaBuilder withCameraDirection(VuforiaLocalizer.CameraDirection cameraDirection) {
+        this.cameraDirection = cameraDirection;
+        return this;
+    }
+
+    public VuforiaBuilder withCameraLocation(OpenGLMatrix cameraLocation) {
+        this.cameraLocation = cameraLocation;
+        return this;
+    }
+
+    public VuforiaBuilder withVuforiaAssetName(String vuforiaAssetName) {
+        this.vuforiaAssetName = vuforiaAssetName;
+        return this;
+    }
+
+    public VuforiaBuilder withVuforiaTrackableNames(String ... vuforiaTrackableNames) {
+        Collection<String> namesCollection = Arrays.asList(vuforiaTrackableNames);
+        this.vuforiaTrackableNames = new ArrayList<>();
+        this.vuforiaTrackableNames.addAll(namesCollection);
+        return this;
+    }
+
+    public VuforiaBuilder withVuforiaTrackableNames(List<String> vuforiaTrackableNames) {
+        this.vuforiaTrackableNames = vuforiaTrackableNames;
+        return this;
+    }
+
+    public VuforiaBuilder withCameraMonitoring(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+        enableCameraMonitoring = true;
+        return this;
+    }
+
+    public Vuforia build() {
+        return new Vuforia(
+                this.cameraDirection,
+                this.enableCameraMonitoring,
+                this.cameraLocation,
+                this.vuforiaKey,
+                this.vuforiaAssetName,
+                this.vuforiaTrackableNames,
+                this.hardwareMap
+        );
+
+    }
+}
