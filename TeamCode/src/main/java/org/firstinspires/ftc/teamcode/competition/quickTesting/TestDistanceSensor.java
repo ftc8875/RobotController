9d74298246339f68376f4fd36ab8ad2e48767640
ftc8875 <ftc8875@gmail.com>
2018-10-31 17:34:52 -0400

    End of meeting 10/31

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/quickTesting/TestDistanceSensor.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.competition.quickTesting;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name = "Test Distance Sensor", group = "test")
+public class TestDistanceSensor extends OpMode {
+
+    DistanceSensor distanceSensor;
+
+    @Override
+    public void init() {
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "color-distance");
+    }
+
+    @Override
+    public void loop() {
+        double distance = distanceSensor.getDistance(DistanceUnit.INCH);
+        if (distance == distanceSensor.distanceOutOfRange) {
+            telemetry.addData("Distance", "OUT OF RANGE");
+        } else {
+            telemetry.addData("Distance", "%.2f", distance);
+        }
+    }
+}
